ddc477171ddae2432a0ce154483b223256cf6bc2
superdev-1018 <cryptopassioner@gmail.com>
2024-10-15 05:32:31 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # solana--lottery-game-backend
 This is backend for solana lottery game.
+=======
+# poolparty backend
+>>>>>>> 24e3207 (Initial commit)
---
+++ src/interfaces/global.ts
@@ -0,0 +1,5 @@
+export const time_frame = [1,3,6,12,24,168,720,2160,4320,8640];
+export const ticket_price = [1,2,3,4,5,6,7,8,9,10];
+export const max_tickets = [56,56,56,56,56,56,56,56,56,56];
+export const dev_fees = [10,9,8,7,6,5,4,3,2,1];
+
